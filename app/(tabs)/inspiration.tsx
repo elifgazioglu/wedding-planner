cf60535eac23a0933808c27387d4e59a789ff9ce
Elif Gazioğlu <103602957+elifgazioglu@users.noreply.github.com>
2024-05-18 18:27:10 +0300

    added routing configuration for modals and screens

--- app/(tabs)/inspiration.tsx
+++ app/(tabs)/inspiration.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router'
 
 const inspiration = () => {
   return (
     <View>
-      <Text>inspiration</Text>
+      <Link href={"/listing/1234"}>Listing</Link>
     </View>
   )
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,14 +4,15 @@ import {
   ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
+import { TouchableOpacity } from "react-native";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { Ionicons } from "@expo/vector-icons";
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
@@ -20,6 +21,8 @@ export default function RootLayout() {
     "mon-sb": require("../assets/fonts/Montserrat-SemiBold.ttf"),
     "mon-b": require("../assets/fonts/Montserrat-Bold.ttf"),
   });
+
+  const router = useRouter();
 
   useEffect(() => {
     if (loaded) {
@@ -34,7 +37,45 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="(modals)/login" options={{ presentation: 'modal', title: 'Log in or sign up'}} />
+      <Stack.Screen
+        name="(modals)/login"
+        options={{
+          title: "Log in or sign up",
+          headerTitleStyle: { fontFamily: "mon-sb" },
+          presentation: "modal",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="close-outline" size={28} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="(modals)/day"
+        options={{
+          title: "Wedding day",
+          headerTitleStyle: { fontFamily: "mon-sb" },
+          presentation: "modal",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="close-outline" size={28} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="(modals)/guests"
+        options={{
+          title: "Guests",
+          headerTitleStyle: { fontFamily: "mon-sb" },
+          presentation: "card",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="close-outline" size={28} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
     </Stack>
   );
 }
